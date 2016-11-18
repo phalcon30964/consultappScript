b12f5c0e0c529cc7c10d15198b5b9a53d21bf098
Christian David Lopez Morcillo <cdlopezmorcillo@gmail.com>
2016-11-17 23:08:38 -0500

    2

--- consultapp.py
+++ consultapp.py
@@ -4,29 +4,23 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/cliente/jhon@gmail.com/123456")
-def hello():
+@app.route("/cliente/<email>/<password>")
+def hello(email,password):
 	ret={}
-	ret['ClienteDTO']=[]
-	client=OrderedDict([('estado', "ACTIVO"),('nombres',"john"),('apellidos',"osorio"),('email',"john@gmail.com"),('password',"123456"),('telefono',"3152543235"),('fechaNacimiento',"05/05/95"),('urlFoto',"http://www.icesi.edu.co/imgs/contenido/png/logo_icesi.png"),('identificacion',"13107019"),('tipoIdentificacion',"CEDULA")])	
-	ret['ClienteDTO'] =(client)
-        return json.dumps(ret)
-	
-@app.route("/cliente/cdlopezmorcillo@gmail.com/1234")
-def hello1():
-	ret={}
-	ret['ClienteDTO']=[]
-	client=OrderedDict([('estado', "ACTIVO"),('nombres',"christian david"),('apellidos',"Lopez morcillo"),('email',"cdlopezmorcillo@gmail.com"),('password',"1234"),('telefono',"3015147161"),('fechaNacimiento',"05/05/95"),('urlFoto',"http://www.icesi.edu.co/imgs/contenido/png/logo_icesi.png"),('identificacion',"1144169711"),('tipoIdentificacion',"TARJETA_DE_IDENTIFICACION")])
-	ret['ClienteDTO'] =(client)
-        return json.dumps(ret)
-	
-@app.route("/cliente/munozm@gmail.com/654321")
-def hello2():
-	ret={}
-	ret['ClienteDTO']=[]
-	client=OrderedDict([('estado', "ACTIVO"),('nombres',"juan david"),('apellidos',"munoz moreno"),('email',"munozm@gmail.com"),('password',"654321"),('telefono',"3173005973"),('fechaNacimiento',"05/05/95"),('urlFoto',"http://www.icesi.edu.co/imgs/contenido/png/logo_icesi.png"),('identificacion',"123456789"),('tipoIdentificacion',"PASAPORTE")])
-	ret['ClienteDTO'] =(client)
-        return json.dumps(ret)
+	if email == "john@gmail.com" and password == "123456":
+		client=OrderedDict([('estado', "ACTIVO"),('nombres',"john"),('apellidos',"osorio"),('email',"john@gmail.com"),('password',"123456"),('telefono',"3152543235"),('fechaNacimiento',"05/05/95"),('urlFoto',"http://www.icesi.edu.co/imgs/contenido/png/logo_icesi.png"),('identificacion',"13107019"),('tipoIdentificacion',"CEDULA")])	
+		ret['ClienteDTO'] =(client)
+		return json.dumps(ret)
+	elif email == "cdlopezmorcillo@gmail.com" and password == "1234":
+		client=OrderedDict([('estado', "ACTIVO"),('nombres',"christian david"),('apellidos',"Lopez morcillo"),('email',"cdlopezmorcillo@gmail.com"),('password',"1234"),('telefono',"3015147161"),('fechaNacimiento',"05/05/95"),('urlFoto',"http://www.icesi.edu.co/imgs/contenido/png/logo_icesi.png"),('identificacion',"1144169711"),('tipoIdentificacion',"TARJETA_DE_IDENTIFICACION")])
+		ret['ClienteDTO'] =(client)
+		return json.dumps(ret)
+	elif email == "munozm@gmail.com" and password == "654321":
+		client=OrderedDict([('estado', "ACTIVO"),('nombres',"juan david"),('apellidos',"munoz moreno"),('email',"munozm@gmail.com"),('password',"654321"),('telefono',"3173005973"),('fechaNacimiento',"05/05/95"),('urlFoto',"http://www.icesi.edu.co/imgs/contenido/png/logo_icesi.png"),('identificacion',"123456789"),('tipoIdentificacion',"PASAPORTE")])
+		ret['ClienteDTO'] =(client)
+		return json.dumps(ret)
+	else:
+		return "clienteNoExiste"
 
 @app.route("/") 
 def hello3():
